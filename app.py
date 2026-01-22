b2c49ad0fccc19510c72cb602e60b1f85333418f
Shubhneet <surbhik0523@gmail.com>
2026-01-23 00:09:09 +0530

    Add role selection and resume input UI

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("AI Career Skill Gap Analyzer - Project Started")
+
+import streamlit as st
+st.title("AI Career Skill-Gap Analyzer")
+st.write("Let's see what you need to reach your goal")
+st.divider()
+#selecting role
+role=st.selectbox("Choose Your Target Role",["Software Developer","Data Analyst","Cybersecurity Engineer","Full Stack Developer","Backend Developer","Frontend Developer"])
+
+#resume input
+resume_text=st.text_area("Please paste your resume",height=200,placeholder="Paste here...")
+
+#temporary action button
+if(st.button("Analyze Skill Gap")):
+    st.success(f"Analyzing resume for the role:{role}")
+    st.info("SKill Gap Analysis logic coming next!")
